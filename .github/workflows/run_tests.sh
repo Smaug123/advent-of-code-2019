8039fae4625fa53535acfb4b0a83c0841090bbc2
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-12-28 01:11:27 +0000

    workflow

---
+++ .github/workflows/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+readarray -d '' crates < <(find . -mindepth 2 -maxdepth 2 -type f -name Cargo.toml -print0)
+for crate in "${crates[@]}"; do
+  name=".#test_$(basename "$(dirname "$crate")")"
+  exit_code=$(nix build "$name")
+  if [ "$exit_code" -ne 0 ]; then
+    nix log "$name"
+    exit "$exit_code"
+  fi
+done
