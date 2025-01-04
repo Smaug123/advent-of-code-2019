25f43c2f2df3dedd261d7cb598375c5156f61237
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-01-04 21:59:27 +0000

    Fix bench

--- day_11/benches/day_11.rs
+++ day_11/benches/day_11.rs
@@ -5,12 +5,12 @@ fn criterion_benchmark(c: &mut Criterion) {
     let input = input(include_str!("../input.txt"));
     c.bench_function("day 11 part 1", |b| {
         b.iter(|| {
-            black_box(part_1(&input));
+            black_box(part_1(&input).unwrap());
         })
     });
     c.bench_function("day 11 part 2", |b| {
         b.iter(|| {
-            black_box(part_2(&input));
+            black_box(part_2(&input).unwrap());
         })
     });
 }
